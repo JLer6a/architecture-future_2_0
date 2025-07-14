44a2e3fe77e56f10abb4f8f2111712963a4443b6
Mikhail Budkin <mbudkin@inno.tech>
2025-07-14 09:35:31 +0300

    begin

---
+++ src/main/java/com/yandex/practicum/architecturefuture_2_0/ArchitectureFuture20Application.java
@@ -0,0 +1,12 @@
+package com.yandex.practicum.architecturefuture_2_0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectureFuture20Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchitectureFuture20Application.class, args);
+    }
+}
---
+++ src/test/java/com/yandex/practicum/architecturefuture_2_0/ArchitectureFuture20ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.yandex.practicum.architecturefuture_2_0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectureFuture20ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
